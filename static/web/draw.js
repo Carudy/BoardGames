750a1ffa2cf81f487b1ab00595ff4d4d66f2dce8
dy <nuaahanzy@gmail.com>
2021-12-11 22:41:49 +0800

    paus

--- static/web/draw.js
+++ static/web/draw.js
@@ -1,6 +1,8 @@
 const server_addr = window.location.hostname || '172.18.101.147'
 const server_port = '6969'
 var server_url = 'http://' + server_addr + ':' + server_port
+var lines = [], send_id = 0
+var player_id = 0, player_type = 0
 
 send = (data, callback)=>{
     data['game_name'] = 'duet'
@@ -16,26 +18,53 @@ send = (data, callback)=>{
 $(() => {
     var ctx = $('canvas')[0].getContext('2d')
     var draw_pos = {x:0, y:0}
+    var color_dict = {
+        '白': '#ccc9cb',
+        '红': '#ff1111',
+        '绿': '#11ff11',
+        '蓝': 'rgb(86,148,199)',
+        '黄': '#ffff11',
+        '紫': '#ff11ff',
+    }
     $('canvas')[0].width = $('#stage').width()
-    $('canvas')[0].height = $('#stage').height()
-    ctx.lineWidth = 5
+    $('canvas')[0].height = $('#stage').height() * 0.9
+    ctx.lineWidth = 3
     ctx.lineCap = 'round'
-    ctx.strokeStyle = '#c0392b'
+    ctx.strokeStyle = '#ccc9cb'
 
     var set_draw_pos = e => {draw_pos.x = e.offsetX; draw_pos.y = e.offsetY;}
+
+    $('.painter').click((e)=>{
+        ctx.strokeStyle = color_dict[e.target.innerText]
+    })
+
     $('canvas').mousedown(e=>{
         set_draw_pos(e)
     })
+
     $('canvas').mouseenter(e=>{
         set_draw_pos(e)
     })
+
     $('canvas').mousemove(e=>{
         if (e.buttons !== 1) return;
         ctx.beginPath()
+        let _line = {
+            'st': [draw_pos.x, draw_pos.y],
+            'color': ctx.strokeStyle,
+        }
         ctx.moveTo(draw_pos.x, draw_pos.y)
         set_draw_pos(e);
         ctx.lineTo(draw_pos.x, draw_pos.y)
+        _line['ed'] = [draw_pos.x, draw_pos.y]
+        lines.push(_line)
         ctx.stroke()
+    })
+
+    $('#reset').click(()=>{
+        lines = []
+        send_id = 0
+        ctx.clearRect(0, 0, $('canvas')[0].width, $('canvas')[0].height)
     })
 
 })
